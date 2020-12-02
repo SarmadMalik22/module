13bfc1e0cfe6c65275f923e31e7c31776e56a799
SarmadMalik22 <lazymalik22@gmail.com>
2020-12-02 01:55:55 -0800

    Initial commit

---
+++ extraddons/hospital/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Hospital Management',
+    'version' : '1.0',
+    'summary': 'Hospital Management Software',
+    'sequence': -100,
+    'description':"""Hospital Management Software """,
+    'category': 'Productivity/Discuss',
+    'website': 'https://www.hospital.com/',
+    'depends' : ['base'],
+    'data': ['views/patient.xml'],
+    'demo': [   ],
+    'qweb': [ ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
